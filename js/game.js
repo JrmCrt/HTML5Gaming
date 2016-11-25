f6f0ca7262d7bb59ed45d6d978582a85b697eb7d
crete_a <crete_a@epitech.eu>
2016-11-25 13:25:43 +0100

    pause correction

--- js/game.js
+++ js/game.js
@@ -530,7 +530,7 @@ function init() {
 		if(keys.fire.includes(key))
 			ship.firing = true;
 
-		if(keys.pause.includes(key))
+		if(keys.pause.includes(key) && game.state.started)
 		{
 			if(!game.state.pause)
 			{
