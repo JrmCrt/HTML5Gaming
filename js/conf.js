4fbec66264a6006757739fd87ede29a31d0a65dc
crete_a <crete_a@epitech.eu>
2016-11-30 12:23:33 +0100

    norm

--- js/conf.js
+++ js/conf.js
@@ -539,7 +539,6 @@ function rand(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function distance(x1, y1, x2, y2)
-{
+function distance(x1, y1, x2, y2){
 	return Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
 }
